a1d6dab263f7fba7634c52acacfd81ef57b61683
Mateus Porto <mateusppp96@gmail.com>
2025-03-10 11:45:26 -0300

    inicio2

---
+++ aps_loja/aps_loja/src/test/java/aps/loja/aps_loja/ApsLojaApplicationTests.java
@@ -0,0 +1,13 @@
+package aps.loja.aps_loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApsLojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
